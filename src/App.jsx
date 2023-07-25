9025607985cf5080e590709fe5a2800ddb7711eb
nicolevenachi <ludwing@unicauca.edu.co>
2023-07-25 13:25:20 -0500

    project basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { actions } from './store'
+
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+
+import "./App.css";
 
 function App() {
-  const counter = useSelector((state) => state.counter)
-  const dispatch = useDispatch()
-
-  const increment = () => {
-    dispatch(
-      // {type: 'INC'}
-      actions.increment()
-    )
-  }
-
-  const decrement = () => {
-    dispatch(
-      // { type: 'DEC'}
-      actions.decrement()
-    )
-  }
-
-  const addBy = () => {
-    dispatch(
-      // { type: 'ADD', payload: 10}
-      actions.addBy(10)
-    )
-  }
-
   return (
-    <>
-
-      <h1> Counter App </h1>
-      <h2>{counter}</h2>
-
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-
-      <button onClick={addBy}>Add by 10</button>
-    </>
-  )
+    <div className="App">
+      <Auth />
+      {/* <Layout /> */}
+    </div>
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/testApp.jsx
@@ -0,0 +1,44 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { actions } from './store/testUsingToolkit'
+
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  const increment = () => {
+    dispatch(
+      // {type: 'INC'}
+      actions.increment()
+    )
+  }
+
+  const decrement = () => {
+    dispatch(
+      // { type: 'DEC'}
+      actions.decrement()
+    )
+  }
+
+  const addBy = () => {
+    dispatch(
+      // { type: 'ADD', payload: 10}
+      actions.addBy(10)
+    )
+  }
+
+  return (
+    <>
+
+      <h1> Counter App </h1>
+      <h2>{counter}</h2>
+
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+
+      <button onClick={addBy}>Add by 10</button>
+    </>
+  )
+}
+
+export default App
+
